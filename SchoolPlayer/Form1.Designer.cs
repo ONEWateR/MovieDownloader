e29c718b0623d4e216abacf4f80270ab7b7baba3
ONEWateR <bawgame1993@gmail.com>
2013-11-27 22:48:22 +0800

    INIT

---
+++ SchoolPlayer/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SchoolPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_url = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_download = new System.Windows.Forms.Button();
+            this.progressBar_download = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.button_folder = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.label_download_info = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_url
+            // 
+            this.textBox_url.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_url.Location = new System.Drawing.Point(14, 49);
+            this.textBox_url.Name = "textBox_url";
+            this.textBox_url.Size = new System.Drawing.Size(404, 23);
+            this.textBox_url.TabIndex = 0;
+            this.textBox_url.Text = "http://na.sise.cn/movie/Video/?dbdd19a6283_1_520.html#playst";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "输入你要下载的在线观看网址：";
+            // 
+            // button_download
+            // 
+            this.button_download.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_download.Location = new System.Drawing.Point(358, 219);
+            this.button_download.Name = "button_download";
+            this.button_download.Size = new System.Drawing.Size(59, 23);
+            this.button_download.TabIndex = 3;
+            this.button_download.Text = "下载";
+            this.button_download.UseVisualStyleBackColor = true;
+            this.button_download.Click += new System.EventHandler(this.button_download_Click);
+            // 
+            // progressBar_download
+            // 
+            this.progressBar_download.Location = new System.Drawing.Point(16, 166);
+            this.progressBar_download.Name = "progressBar_download";
+            this.progressBar_download.Size = new System.Drawing.Size(404, 23);
+            this.progressBar_download.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(13, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "下载进度：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(13, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "保存路径：";
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_path.Location = new System.Drawing.Point(15, 107);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(350, 23);
+            this.textBox_path.TabIndex = 0;
+            this.textBox_path.Text = "E:\\";
+            // 
+            // button_folder
+            // 
+            this.button_folder.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_folder.Location = new System.Drawing.Point(369, 106);
+            this.button_folder.Name = "button_folder";
+            this.button_folder.Size = new System.Drawing.Size(49, 23);
+            this.button_folder.TabIndex = 7;
+            this.button_folder.Text = "浏览";
+            this.button_folder.UseVisualStyleBackColor = true;
+            this.button_folder.Click += new System.EventHandler(this.button_folder_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(182, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "ONEWateR";
+            // 
+            // label_download_info
+            // 
+            this.label_download_info.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label_download_info.Location = new System.Drawing.Point(16, 196);
+            this.label_download_info.Name = "label_download_info";
+            this.label_download_info.Size = new System.Drawing.Size(401, 15);
+            this.label_download_info.TabIndex = 8;
+            this.label_download_info.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(441, 273);
+            this.Controls.Add(this.label_download_info);
+            this.Controls.Add(this.button_folder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar_download);
+            this.Controls.Add(this.button_download);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_path);
+            this.Controls.Add(this.textBox_url);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "内网电影下载器 ver 1.1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_url;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_download;
+        private System.Windows.Forms.ProgressBar progressBar_download;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_path;
+        private System.Windows.Forms.Button button_folder;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Label label_download_info;
+    }
+}
+
---
+++ SchoolPlayer/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace SchoolPlayer
+{
+    public partial class Form1 : Form
+    {
+        WebBrowser wb = new WebBrowser();
+        string set = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+            set = System.Environment.CurrentDirectory + "\\set.ini";
+            /*
+            if (!File.Exists(set)) {
+                File.Create(set);
+            }
+            */
+            textBox_path.Text = ContentValue("Set", "path");
+        }
+
+
+        /// <summary>
+        /// 写入INI文件
+        /// </summary>
+        /// <param name="section">节点名称[如[TypeName]]</param>
+        /// <param name="key">键</param>
+        /// <param name="val">值</param>
+        /// <param name="filepath">文件路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filepath);
+        /// <summary>
+        /// 读取INI文件
+        /// </summary>
+        /// <param name="section">节点名称</param>
+        /// <param name="key">键</param>
+        /// <param name="def">值</param>
+        /// <param name="retval">stringbulider对象</param>
+        /// <param name="size">字节大小</param>
+        /// <param name="filePath">文件路径</param>
+        /// <returns></returns>
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retval, int size, string filePath);
+
+        private string strFilePath = Application.StartupPath + "\\FileConfig.ini";//获取INI文件路径
+
+
+        /// <summary>
+        /// 自定义读取INI文件中的内容方法
+        /// </summary>
+        /// <param name="Section">键</param>
+        /// <param name="key">值</param>
+        /// <returns></returns>
+        private string ContentValue(string Section, string key)
+        {
+
+            StringBuilder temp = new StringBuilder(1024);
+            GetPrivateProfileString(Section, key, "", temp, 1024, set);
+            return temp.ToString();
+        }
+
+        /// <summary>
+        /// 下载页面，获取url进行下载
+        /// </summary>
+        /// <param name="movie_url"></param>
+        private void DownloadPage(string movie_url) {
+            try
+            {
+                wb.DocumentCompleted += wb_DocumentCompleted;
+                wb.Url = new Uri(movie_url);
+            }
+            catch (Exception e) {
+                MessageBox.Show("网址有问题");
+            }
+        }
+
+        private void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {           
+            foreach (HtmlElement he in wb.Document.GetElementsByTagName("param"))
+            {
+                if (he.GetAttribute("name") == "url")
+                {
+                    wb.Dispose();
+                    DownloadMovie(he.GetAttribute("value"));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 下载电影
+        /// </summary>
+        /// <param name="StringUrl">电影下载地址</param>
+        private void DownloadMovie(string StringUrl) {
+
+            string path = textBox_path.Text;
+
+            // 获取电影的文件名称
+            String MovieName = StringUrl.Split('/')[StringUrl.Split('/').Length - 1];
+            // 获取Host
+            String host = StringUrl.Split('/')[2];
+
+            Uri url = new Uri(StringUrl + "?agent=powerpxp&session=-1392595589");
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+            request.Method = "GET";
+
+            request.UserAgent = "Novasoft NetPlayer/4.0";
+
+            request.Host = host;
+
+
+            FileStream f = null;
+            try
+            {
+                f = new FileStream(path + MovieName, FileMode.Create);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("路径有问题。");
+            }
+
+            using (HttpWebResponse webResponse = (HttpWebResponse)request.GetResponse())
+            {
+
+                long dataLengthHaveRead  = 0;
+                long dataLength  = webResponse.ContentLength;
+
+                //MessageBox.Show(webResponse.StatusCode.ToString());
+
+                if (webResponse.StatusCode.ToString().Equals("OK"))
+                {
+                    WritePrivateProfileString("Set", "path", folderBrowserDialog.SelectedPath, set);
+                }else{
+                    MessageBox.Show("获取不了的说~~");
+                }
+
+                using (Stream stream = webResponse.GetResponseStream())
+                {
+
+                    const long ChunkSize = 102400; //100K 每次读取文件，只读取100K，这样可以缓解服务器的压力
+                    byte[] buffer = new byte[ChunkSize];
+
+                    while (dataLength - dataLengthHaveRead > 0)
+                    {
+                        Application.DoEvents();
+                        int lengthRead = stream.Read(buffer, 0, Convert.ToInt32(ChunkSize));
+                        dataLengthHaveRead += lengthRead;
+                        double pro = Math.Round(Convert.ToDouble(dataLengthHaveRead) / dataLength, 3) * 100;
+                        progressBar_download.Value = Convert.ToInt32(pro);
+                        label_download_info.Text = pro + "%";
+                        //MessageBox.Show(Convert.ToDouble(dataLengthHaveRead) / dataLength * 100 + " " + dataLength, "" + lengthRead);
+                        f.Write(buffer, 0, lengthRead);
+                        f.Flush();
+                    }
+                    f.Close();
+                    MessageBox.Show("下载完成~！ > <");
+                    wb = new WebBrowser();
+                    progressBar_download.Value = 0;
+                }
+            }
+
+        }
+
+        private void button_download_Click(object sender, EventArgs e)
+        {
+            DownloadPage(textBox_url.Text);
+        }
+
+        private void button_folder_Click(object sender, EventArgs e)
+        {
+
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                string path = folderBrowserDialog.SelectedPath;
+                textBox_path.Text = path.EndsWith("\\") ? path : path+"\\";
+            }
+        }
+
+
+
+
+
+    }
+}
